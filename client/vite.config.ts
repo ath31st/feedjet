13d97bb65d97867c767eb6fd70aa54b71695283b
ath31st <ath31st@yandex.ru>
2025-07-19 09:15:48 +0300

    add types/node dependency, update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
     host: true,
   },
   plugins: [react(), tailwindcss()],
+    resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
