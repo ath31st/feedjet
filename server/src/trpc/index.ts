f2673ae1d0c4bf4390466f03aa67fe2e5106eb7d
ath31st <ath31st@yandex.ru>
2025-07-22 20:04:51 +0300

    remove fastify shit

--- server/src/trpc/index.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-import { userRoutes } from './routes/users.js';
-
-export async function routes(app: FastifyInstance) {
-  app.register(userRoutes, { prefix: '/api/users' });
-  //app.register(configs, { prefix: '/api/configs' });
-}
