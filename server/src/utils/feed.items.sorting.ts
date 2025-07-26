4b72c125a4a5351bca08035749abde17996aab57
ath31st <ath31st@yandex.ru>
2025-07-26 18:14:58 +0300

    fix sorting feed item method

--- server/src/utils/feed.items.sorting.ts
+++ server/src/utils/feed.items.sorting.ts
@@ -1,9 +1,7 @@
 import type { FeedItem } from '@shared/types/feed.js';
 
 export function sortFeedItemsByDateDescending(feedItems: FeedItem[]) {
-  const sortedItems = feedItems.sort(
-    (a, b) => new Date(a.pubDate).getTime() - new Date(b.pubDate).getTime(),
+  return [...feedItems].sort(
+    (a, b) => new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime(),
   );
-
-  return sortedItems;
 }
