369e16a8d060d5d919152259f3ddfae707d6c00f
ath31st <ath31st@yandex.ru>
2025-07-30 18:53:13 +0300

    transfer animated background to kiosk page

--- client/src/app/layouts/RootLayout.tsx
+++ client/src/app/layouts/RootLayout.tsx
@@ -1,18 +1,14 @@
 import { Outlet } from 'react-router-dom';
-import { AnimatedSigmaBackground } from '@/shared/ui/AnimatedSigmaBackground ';
 import { ThemeProvider } from '../providers/ThemeProvider';
 import { TrpcProvider } from '../providers/TrpcProvider';
 import { Initializer } from '../init/Initializer';
 import { ToasterConfig } from '@/shared/ui/ToasterConfig';
-//import { BackgroundAnimation } from '@/shared/ui/BackgroundAnimation';
 
 export function RootLayout() {
   return (
     <ThemeProvider>
       <TrpcProvider>
         <Initializer />
-        <AnimatedSigmaBackground />
-        {/* <BackgroundAnimation /> */}
         <ToasterConfig />
         <Outlet />
       </TrpcProvider>
--- client/src/pages/KioskPage.tsx
+++ client/src/pages/KioskPage.tsx
@@ -1,8 +1,12 @@
+import { AnimatedSigmaBackground } from '@/shared/ui/AnimatedSigmaBackground ';
 import { FeedWidget } from '../widgets/feed';
+//import { BackgroundAnimation } from '@/shared/ui/BackgroundAnimation';
 
 export function KioskPage() {
   return (
     <div className="h-screen w-screen p-4">
+      <AnimatedSigmaBackground />
+      {/* <BackgroundAnimation /> */}
       <FeedWidget />
     </div>
   );
