2ed3be6b211ac640d1158c77cd9e622a754e8588
ath31st <ath31st@yandex.ru>
2025-08-02 08:59:07 +0300

    remove 4k styles from 401 and 404 pages

--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -2,7 +2,7 @@ export function NotFoundPage() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div
-        className="4k:rounded-4xl rounded-xl 4k:border-4 border-2 4k:p-28 p-12 4k:text-8xl text-2xl"
+        className="rounded-xl border-2 4k:p-28 p-12 text-2xl"
         style={{
           borderColor: 'var(--border)',
           backgroundColor: 'var(--card-bg)',
--- client/src/pages/UnauthorizedPage.tsx
+++ client/src/pages/UnauthorizedPage.tsx
@@ -4,16 +4,16 @@ export function UnauthorizedPage() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div
-        className="flex flex-col items-center gap-8 4k:rounded-4xl rounded-xl 4k:p-28 p-12"
+        className="flex flex-col items-center gap-8 rounded-xl p-12"
         style={{
           borderColor: 'var(--border)',
           backgroundColor: 'var(--card-bg)',
         }}
       >
-        <h1 className="4k:text-8xl text-4xl">Требуется авторизация</h1>
+        <h1 className="text-4xl">Требуется авторизация</h1>
         <Link
           to="/login"
-          className="4k:text-5xl text-2xl underline transition-opacity hover:opacity-80"
+          className="text-2xl underline transition-opacity hover:opacity-80"
         >
           Перейти на страницу входа
         </Link>
