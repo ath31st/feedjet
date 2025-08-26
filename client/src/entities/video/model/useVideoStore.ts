e6dc25975c06945814ab84c50c2a696301693a29
ath31st <ath31st@yandex.ru>
2025-08-26 11:25:27 +0300

    rework video store

--- client/src/entities/video/model/useVideoStore.ts
+++ client/src/entities/video/model/useVideoStore.ts
@@ -1,20 +1,34 @@
 import { create } from 'zustand';
-
-type Video = {
-  id: number;
-  name: string;
-  url: string;
-};
+import type { VideoMetadata } from '..';
+import { trpcClient } from '@/shared/api/trpc';
 
 type VideoStore = {
-  videos: Video[];
-  addVideo: (video: Video) => void;
-  removeVideo: (id: number) => void;
+  videos: VideoMetadata[];
+  currentVideo: VideoMetadata | null;
+  loading: boolean;
+  error: string | null;
+  initStore: () => Promise<void>;
+  setVideos: (videos: VideoMetadata[]) => void;
+  setCurrentVideo: (video: VideoMetadata | null) => void;
 };
 
 export const useVideoStore = create<VideoStore>((set) => ({
   videos: [],
-  addVideo: (video) => set((s) => ({ videos: [...s.videos, video] })),
-  removeVideo: (id) =>
-    set((s) => ({ videos: s.videos.filter((v) => v.id !== id) })),
+  currentVideo: null,
+  loading: false,
+  error: null,
+  initStore: async () => {
+    set({ loading: true });
+    try {
+      const data = await trpcClient.videoFile.listFiles.query();
+      set({ videos: data, error: null, loading: false });
+    } catch (err) {
+      set({
+        error: err instanceof Error ? err.message : 'Unknown error',
+        loading: false,
+      });
+    }
+  },
+  setVideos: (videos) => set({ videos }),
+  setCurrentVideo: (video) => set({ currentVideo: video }),
 }));
