d5730215c6ca59c694915b5a44ef95ef1cee3d87
ath31st <ath31st@yandex.ru>
2025-09-06 20:41:45 +0300

    fix constructor for service error parrent class

--- server/src/errors/service.error.ts
+++ server/src/errors/service.error.ts
@@ -1,10 +1,10 @@
 export type ServiceErrorCode = 400 | 401 | 403 | 404 | 409 | 500;
 
 export class ServiceError extends Error {
-  constructor(
-    public readonly code: ServiceErrorCode,
-    message: string,
-  ) {
+  public readonly code: ServiceErrorCode;
+
+  constructor(code: ServiceErrorCode, message: string) {
     super(message);
+    this.code = code;
   }
 }
