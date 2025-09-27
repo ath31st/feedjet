a83a369091d86573f0353a2fcbf6d85d2491f839
ath31st <ath31st@yandex.ru>
2025-09-27 20:51:06 +0300

    add kiosk trpc routes

---
+++ server/src/trpc/routes/kiosk.route.ts
@@ -0,0 +1,43 @@
+import { t, kioskService, eventBus, publicProcedure } from '../../container.js';
+import { handleServiceCall } from '../error.handler.js';
+import {
+  kioskIdInputSchema,
+  kioskSlugInputSchema,
+  newKioskSchema,
+} from '../../validations/schemas/kiosk.schemas.js';
+import { protectedProcedure } from '../../middleware/auth.js';
+
+export const kioskRouter = t.router({
+  getAll: publicProcedure.query(() => {
+    return kioskService.getAll();
+  }),
+
+  getBySlug: publicProcedure.input(kioskSlugInputSchema).query(({ input }) => {
+    return handleServiceCall(() => kioskService.getBySlug(input.slug));
+  }),
+
+  create: protectedProcedure.input(newKioskSchema).mutation(({ input }) =>
+    handleServiceCall(() => {
+      const kiosk = kioskService.create(input);
+      eventBus.emit('kiosk-created', kiosk);
+      return kiosk;
+    }),
+  ),
+
+  delete: protectedProcedure.input(kioskIdInputSchema).mutation(({ input }) =>
+    handleServiceCall(() => {
+      kioskService.delete(input.kioskId);
+      return { success: true };
+    }),
+  ),
+
+  deleteBySlug: protectedProcedure
+    .input(kioskSlugInputSchema)
+    .mutation(({ input }) =>
+      handleServiceCall(() => {
+        kioskService.deleteBySlug(input.slug);
+        eventBus.emit('kiosk-deleted', { slug: input.slug });
+        return { success: true };
+      }),
+    ),
+});
