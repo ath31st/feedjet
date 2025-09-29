1bcd4f87a2b62c394d365a0cd3cb04b144f2f291
ath31st <ath31st@yandex.ru>
2025-09-29 16:06:13 +0300

    update kiosk management with new feature

--- client/src/widgets/kiosk-management/KioskManagement.tsx
+++ client/src/widgets/kiosk-management/KioskManagement.tsx
@@ -1,15 +1,47 @@
-import { useGetAllKiosks, useKioskStore } from '@/entities/kiosk';
+import { useState } from 'react';
+import {
+  useGetAllKiosks,
+  useKioskStore,
+  useCreateKiosk,
+} from '@/entities/kiosk';
+import { CreateKioskDialog } from '@/features/kiosk-create';
 import { KioskSelector } from '@/features/kiosk-selector';
+import type { NewKiosk } from '@/entities/kiosk';
+import { IconButton } from '@/shared/ui/common/IconButton';
+import { PlusIcon } from '@radix-ui/react-icons';
 
 export function KioskManagement() {
   const { setCurrentKiosk, currentKiosk } = useKioskStore();
   const { data: kiosks = [] } = useGetAllKiosks();
+  const createKioskMutation = useCreateKiosk();
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleCreateKiosk = (data: NewKiosk) => {
+    createKioskMutation.mutate(data, {
+      onSuccess: (newKiosk) => {
+        setCurrentKiosk(newKiosk);
+        setIsDialogOpen(false);
+      },
+    });
+  };
 
   return (
-    <KioskSelector
-      kiosks={kiosks}
-      activeKiosk={currentKiosk}
-      onChange={setCurrentKiosk}
-    />
+    <>
+      <div className="flex items-center gap-4">
+        <KioskSelector
+          kiosks={kiosks}
+          activeKiosk={currentKiosk}
+          onChange={setCurrentKiosk}
+        />
+        <IconButton onClick={() => setIsDialogOpen(true)} icon={<PlusIcon />} />
+      </div>
+
+      <CreateKioskDialog
+        open={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+        onCreate={handleCreateKiosk}
+      />
+    </>
   );
 }
