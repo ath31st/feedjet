a99d67872eca1c8ccec5c039c5aafcaeea250ef4
ath31st <ath31st@yandex.ru>
2025-09-30 15:06:52 +0300

    update kiosk type (uncomment dates)

--- shared/types/kiosk.ts
+++ shared/types/kiosk.ts
@@ -5,8 +5,8 @@ export interface Kiosk {
   description?: string | null;
   location?: string | null;
   isActive: boolean;
-  //  createdAt: Date;
-  //  updatedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface NewKiosk {
