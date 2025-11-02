dc5b932de4776a53ec642eab594c8561379d54ca
ath31st <ath31st@yandex.ru>
2025-11-02 13:02:48 +0300

    update birthday list and card

--- client/src/widgets/birthdays-management/ui/BirthdayCard.tsx
+++ client/src/widgets/birthdays-management/ui/BirthdayCard.tsx
@@ -1,12 +1,15 @@
+import { IconButton } from '@/shared/ui/common';
+import { Cross1Icon } from '@radix-ui/react-icons';
 import type { Birthday } from '@shared/types/birthdays';
 
 interface BirthdayCardProps {
   birthday: Birthday;
+  onDelete: (id: number) => void;
 }
 
-export function BirthdayCard({ birthday }: BirthdayCardProps) {
+export function BirthdayCard({ birthday, onDelete }: BirthdayCardProps) {
   return (
-    <div className="flex items-center justify-between rounded-lg border border-[var(--border)] bg-[var(--card-bg)] p-4">
+    <div className="flex items-center justify-between rounded-lg border border-[var(--border)] bg-[var(--card-bg)] p-2">
       <div className="truncate font-semibold text-[var(--text-primary)]">
         {birthday.fullName}
       </div>
@@ -20,6 +23,11 @@ export function BirthdayCard({ birthday }: BirthdayCardProps) {
             {birthday.department}
           </span>
         )}
+        <IconButton
+          onClick={() => onDelete(birthday.id)}
+          tooltip="Удалить день рождения"
+          icon={<Cross1Icon className="h-4 w-4 cursor-pointer" />}
+        />
       </div>
     </div>
   );
--- client/src/widgets/birthdays-management/ui/BirthdayList.tsx
+++ client/src/widgets/birthdays-management/ui/BirthdayList.tsx
@@ -1,17 +1,25 @@
-import { useGetAllBirthdays } from '@/entities/birthday';
+import { useDeleteBirthday, useGetAllBirthdays } from '@/entities/birthday';
 import { LoadingThreeDotsJumping } from '@/shared/ui';
 import { BirthdayCard } from './BirthdayCard';
 
 export function BirthdayList() {
   const { isLoading, data: birthdays } = useGetAllBirthdays();
+  const deleteBirthday = useDeleteBirthday();
+  const handleDelete = (id: number) => {
+    deleteBirthday.mutate({ id });
+  };
 
   if (isLoading) return <LoadingThreeDotsJumping />;
   if (!birthdays?.length) return <p>В базе данных нет дней рождения</p>;
 
   return (
-    <div className="flex w-full flex-col gap-4">
+    <div className="flex w-full flex-col gap-2">
       {birthdays.map((birthday) => (
-        <BirthdayCard key={birthday.id} birthday={birthday} />
+        <BirthdayCard
+          key={birthday.id}
+          birthday={birthday}
+          onDelete={handleDelete}
+        />
       ))}
     </div>
   );
