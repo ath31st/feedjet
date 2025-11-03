7f1e45b64eec8ff77fe24f5b3fe5dbb7680153aa
ath31st <ath31st@yandex.ru>
2025-11-03 11:07:08 +0300

    add date format constants

---
+++ client/src/shared/constant/dateFormat.ts
@@ -0,0 +1,12 @@
+export const DATE_FORMATS = [
+  { label: 'Стандартный', example: '01.11.2025' },
+  { label: 'dd/MM/yyyy', example: '01/11/2025' },
+  { label: 'dd-MM-yyyy', example: '01-11-2025' },
+  { label: 'MM/dd/yyyy', example: '11/01/2025' },
+  { label: 'MM-dd-yyyy', example: '11-01-2025' },
+  { label: 'yyyy-MM-dd', example: '2025-11-01' },
+  { label: 'yyyy/MM/dd', example: '2025/11/01' },
+  { label: 'yyyy.MM.dd', example: '2025.11.01' },
+  { label: 'dd.MM.yy', example: '01.11.25' },
+  { label: 'MM/dd/yy', example: '11/01/25' },
+];
--- client/src/shared/constant/index.ts
+++ client/src/shared/constant/index.ts
@@ -1,1 +1,2 @@
 export * from './hours';
+export * from './dateFormat';
