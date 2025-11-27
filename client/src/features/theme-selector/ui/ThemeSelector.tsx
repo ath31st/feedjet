99ab795fbc0082ff208c45dedf00be8848d14275
ath31st <ath31st@yandex.ru>
2025-11-27 13:10:01 +0300

    change theme selector

--- client/src/features/theme-selector/ui/ThemeSelector.tsx
+++ client/src/features/theme-selector/ui/ThemeSelector.tsx
@@ -1,6 +1,6 @@
 import { useThemeSelector } from '../model/useThemeSelector';
 import { themes } from '@shared/types/ui.config';
-import { SimpleDropdownMenu } from '@/shared/ui/common';
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
 
 interface ThemeSelectorProps {
   kioskId: number;
@@ -13,12 +13,27 @@ export function ThemeSelector({ kioskId }: ThemeSelectorProps) {
 
   return (
     <div className="flex flex-col gap-1">
-      <span className="text-[var(--text)]">Тема:</span>
-      <SimpleDropdownMenu
+      <span className="text-[var(--text)]">Цветовая тема:</span>
+
+      <ToggleGroup.Root
+        type="single"
+        className="grid grid-cols-4 gap-1 rounded-lg border border-[var(--border)] p-1"
         value={theme}
-        options={themes}
-        onSelect={(t) => handleThemeChange(kioskId, t)}
-      />
+        onValueChange={(next) => {
+          if (!next) return;
+          handleThemeChange(kioskId, next);
+        }}
+      >
+        {themes.map((t) => (
+          <ToggleGroup.Item
+            key={t}
+            value={t}
+            className="cursor-pointer rounded-lg px-2 py-1 hover:bg-[var(--button-hover-bg)] data-[state=on]:bg-[var(--button-bg)]"
+          >
+            {t}
+          </ToggleGroup.Item>
+        ))}
+      </ToggleGroup.Root>
     </div>
   );
 }
