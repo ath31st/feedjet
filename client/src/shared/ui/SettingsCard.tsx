62145bb49e110f0932e73273bdc9345f88af3619
ath31st <ath31st@yandex.ru>
2025-11-27 13:11:03 +0300

    add background gradient for settings card title

--- client/src/shared/ui/SettingsCard.tsx
+++ client/src/shared/ui/SettingsCard.tsx
@@ -15,7 +15,18 @@ export function SettingsCard({
     <section
       className={`rounded-lg bg-[var(--card-bg)] p-6 shadow-2xl ${className}`}
     >
-      {title && <h2 className="mb-4 font-semibold text-xl">{title}</h2>}
+      {/* {title && <h2 className="mb-4 font-semibold text-xl">{title}</h2>} */}
+      {title && (
+        <h2
+          className="mb-4 inline-block w-full rounded-lg px-2 py-2 font-semibold text-xl"
+          style={{
+            background:
+              'linear-gradient(to right, var(--button-bg), var(--card-bg) 90%)',
+          }}
+        >
+          {title}
+        </h2>
+      )}
       {children}
     </section>
   );
