92799be056376150466cac16e777d29e818919df
ath31st <ath31st@yandex.ru>
2025-12-01 13:22:38 +0300

    change path to fallback images

--- client/src/shared/lib/getRandomFallbackImage.ts
+++ client/src/shared/lib/getRandomFallbackImage.ts
@@ -2,12 +2,12 @@ export function getRandomFallbackImage(stringForHash?: string): string {
   const maxImages = 35;
   if (!stringForHash) {
     const randomIndex = Math.floor(Math.random() * maxImages) + 1;
-    return `/images/${randomIndex}.webp`;
+    return `/fallback-images/${randomIndex}.webp`;
   }
 
   const hash = stringForHash
     .split('')
     .reduce((acc, c) => acc + c.charCodeAt(0), 0);
   const index = (hash % maxImages) + 1;
-  return `/images/${index}.webp`;
+  return `/fallback-images/${index}.webp`;
 }
