8a7d7db8cad7b98cc29909a6837fb73809272c26
ath31st <ath31st@yandex.ru>
2026-04-12 10:43:30 +0300

    add common sidebar panel (with hover panel)

--- client/src/shared/ui/index.ts
+++ client/src/shared/ui/index.ts
@@ -23,3 +23,4 @@ export { KioskForm } from './KioskForm';
 export { IntegrationForm } from './IntegrationForm';
 export { SeasonOverlay } from './season-overlay/SeasonOverlay';
 export { BirthdayTransformView } from './BirthdayTransformView';
+export { SidebarPanel } from './common/SidebarPanel';
