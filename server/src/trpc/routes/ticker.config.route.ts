42c48d8bf5440e700da34b5e0e9ba7ff4fa4904d
ath31st <ath31st@yandex.ru>
2026-05-01 10:34:36 +0300

    add ticker routes

---
+++ server/src/trpc/routes/ticker.config.route.ts
@@ -0,0 +1,46 @@
+import { t, tickerConfigService } from '../../container.js';
+import { handleServiceCall } from '../error.handler.js';
+import { kioskIdInputSchema } from '../../validations/schemas/kiosk.schemas.js';
+import { protectedProcedure, publicProcedure } from '../../middleware/auth.js';
+import {
+  tickerConfigCreateInputSchema,
+  tickerConfigUpdateInputSchema,
+} from '../../validations/schemas/ticker.config.schemas.js';
+
+export const tickerConfigRouter = t.router({
+  getAll: publicProcedure.query(() => {
+    return handleServiceCall(() => {
+      return tickerConfigService.getAll();
+    });
+  }),
+
+  getByKioskId: publicProcedure.input(kioskIdInputSchema).query(({ input }) => {
+    return handleServiceCall(() => {
+      return tickerConfigService.get(input.kioskId);
+    });
+  }),
+
+  create: protectedProcedure
+    .input(tickerConfigCreateInputSchema)
+    .mutation(({ input }) => {
+      return handleServiceCall(() => {
+        const { kioskId, data } = input;
+        return tickerConfigService.create(kioskId, data);
+      });
+    }),
+
+  update: protectedProcedure
+    .input(tickerConfigUpdateInputSchema)
+    .mutation(({ input }) => {
+      return handleServiceCall(() => {
+        const { kioskId, data } = input;
+        return tickerConfigService.update(kioskId, data);
+      });
+    }),
+
+  delete: protectedProcedure.input(kioskIdInputSchema).mutation(({ input }) => {
+    return handleServiceCall(() => {
+      return tickerConfigService.delete(input.kioskId);
+    });
+  }),
+});
