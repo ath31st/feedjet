c9ddc69eb16d54db519cdc5ed2b407e20efa3882
ath31st <ath31st@yandex.ru>
2026-05-17 15:47:52 +0300

    update base action style

--- client/src/shared/styles/baseActionClass.ts
+++ client/src/shared/styles/baseActionClass.ts
@@ -1,2 +1,3 @@
 export const baseActionClass =
-  'w-full rounded-lg border border-(--border) bg-(--card-bg) p-2 text-center text-xs transition-colors hover:bg-(--button-hover-bg)';
+  'w-full rounded-lg border border-(--border) bg-(--card-bg) p-2 text-center text-sm transition-colors hover:bg-(--button-hover-bg) ' +
+  'data-[state=active]:bg-(--button-bg) data-[state=active]:text-(--text) data-[state=active]:border-(--border)';
