662c4c1ef2d267757642212c7980a1b62b042346
ath31st <ath31st@yandex.ru>
2026-05-20 11:23:40 +0300

    update uiConfigStore and types in index file

--- client/src/entities/ui-config/index.ts
+++ client/src/entities/ui-config/index.ts
@@ -3,7 +3,18 @@ export type {
   widgetTypes,
   Theme,
   UiConfig,
+  ScreenRotation,
+  AnimationType,
+  SeasonOverlayMode,
 } from '@shared/types/ui.config';
-export { themes, themesFull } from '@shared/types/ui.config';
+export {
+  themes,
+  themesFull,
+  screenRotations,
+  animationTypes,
+  animationLabels,
+  seasonOverlayModes,
+  seasonOverlayLabels,
+} from '@shared/types/ui.config';
 export { useUiConfigStore } from './model/uiConfigStore';
 export * from './api/useUiConfig';
