8c01fa51470532545499132dc776d034223abc85
ath31st <ath31st@yandex.ru>
2026-05-20 11:21:22 +0300

    clear kioskParams hook

--- client/src/features/kiosk-params/model/useKioskParams.ts
+++ client/src/features/kiosk-params/model/useKioskParams.ts
@@ -1,12 +1,6 @@
-import { useParams, useSearchParams } from 'react-router-dom';
-import { parseRotateParam } from '@/shared/lib/parseRotateParam';
-import { parseAnimationParam } from '@/shared/lib/parseAnimationParam';
+import { useParams } from 'react-router-dom';
 
 export function useKioskParams() {
   const { slug = 'default' } = useParams();
-  const [searchParams] = useSearchParams();
-  const rotate = parseRotateParam(searchParams.get('rotate'));
-  const animation = parseAnimationParam(searchParams.get('animation'));
-
-  return { rotate, animation, slug };
+  return { slug };
 }
