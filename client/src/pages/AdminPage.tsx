7d3daf76f9ad1a59822aa5644a47d480413521a6
ath31st <ath31st@yandex.ru>
2026-06-09 14:41:18 +0300

    fix render help panel for admin page

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -200,40 +200,37 @@ export function AdminPage() {
 
       <Tabs.Root value={tab} onValueChange={setTab}>
         <div className="flex items-start gap-6">
-          <div className="flex flex-1 gap-6">
-            <Tabs.List
-              className="scrollbar-hide flex w-60 shrink-0 flex-col"
-              aria-label="Управление админ-панелью"
+          <Tabs.List
+            className="scrollbar-hide flex w-60 shrink-0 flex-col"
+            aria-label="Управление админ-панелью"
+          >
+            <div className="flex flex-col gap-2">
+              {adminTabs.map((t) => (
+                <AdminTabTrigger key={t.value} value={t.value} icon={t.icon}>
+                  {t.label}
+                </AdminTabTrigger>
+              ))}
+            </div>
+          </Tabs.List>
+
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={tab}
+              className="flex min-w-0 flex-1 gap-6"
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.25 }}
             >
-              <div className="flex flex-col gap-2">
-                {adminTabs.map((t) => (
-                  <AdminTabTrigger key={t.value} value={t.value} icon={t.icon}>
-                    {t.label}
-                  </AdminTabTrigger>
-                ))}
-              </div>
-            </Tabs.List>
+              <div className="min-w-0 flex-1">{renderTab()}</div>
 
-            <div className="relative min-w-0 flex-1">
-              <AnimatePresence mode="wait">
-                <motion.div
-                  key={tab}
-                  initial={{ opacity: 0, y: 10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: -10 }}
-                  transition={{ duration: 0.25 }}
-                >
-                  {renderTab()}
-                </motion.div>
-              </AnimatePresence>
-            </div>
-          </div>
-
-          {showHelp && (
-            <div className="flex w-60 flex-col">
-              <AdminHelpPanel helpItems={helpItems} />
-            </div>
-          )}
+              {showHelp && (
+                <div className="flex w-60 shrink-0 flex-col">
+                  <AdminHelpPanel helpItems={helpItems} />
+                </div>
+              )}
+            </motion.div>
+          </AnimatePresence>
         </div>
       </Tabs.Root>
     </div>
