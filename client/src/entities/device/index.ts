2a7d698bca8418f497db11b92d235355f31036cc
ath31st <ath31st@yandex.ru>
2026-06-22 15:40:10 +0300

    update admin page with device tab

--- client/src/entities/device/index.ts
+++ client/src/entities/device/index.ts
@@ -1,3 +1,9 @@
 export * from './api/useDevice';
 export * from '@shared/types/device';
 export * from './model/deviceStore';
+import type { Device as DeviceApi } from '@shared/types/device';
+
+export type Device = Omit<DeviceApi, 'firstSeenAt' | 'lastSeenAt'> & {
+  firstSeenAt: string;
+  lastSeenAt: string;
+};
