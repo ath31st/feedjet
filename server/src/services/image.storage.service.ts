480822055a14287c8c48001a136c71a36fbe217b
ath31st <ath31st@yandex.ru>
2026-07-05 11:39:27 +0300

    refactoring image storage service

--- server/src/services/image.storage.service.ts
+++ server/src/services/image.storage.service.ts
@@ -1,82 +1,45 @@
-import { FileStorageService } from './file.storage.service.js';
-import path from 'node:path';
-import { promises as fs } from 'node:fs';
-import sharp from 'sharp';
-import { FileStorageError } from '../errors/file.storage.error.js';
 import type { ImageMetadata } from '@shared/types/image.js';
-import { webReadableToNode } from '../utils/stream.js';
 import { ImageStorageServiceError } from '../errors/image.error.js';
 import type { DbType } from '../container.js';
 import { imagesTable } from '../db/schema.js';
 import { eq } from 'drizzle-orm';
+import { BaseImageStorageService } from './base.image.storage.service.js';
 
-export class ImageStorageService extends FileStorageService {
+export class ImageStorageService extends BaseImageStorageService {
   private readonly db: DbType;
-  private readonly imageDir = 'images';
-  private readonly allowedExtensions = [
-    '.jpg',
-    '.JPG',
-    '.jpeg',
-    '.JPEG',
-    '.png',
-    '.PNG',
-    '.webp',
-    '.WEBP',
-    '.bmp',
-    '.BMP',
-  ];
 
   constructor(
     db: DbType,
     baseDir: string,
     loggerName: string = 'imageStorageService',
   ) {
-    super(baseDir, loggerName);
+    super(baseDir, 'images', loggerName);
     this.db = db;
-    fs.mkdir(path.join(this.baseDir, this.imageDir), { recursive: true });
-  }
-
-  protected override getFilePath(fileName: string) {
-    return path.join(this.baseDir, this.imageDir, fileName);
-  }
-
-  override async listFiles(): Promise<string[]> {
-    const dir = path.join(this.baseDir, this.imageDir);
-    return fs.readdir(dir);
-  }
-
-  override getBaseDir() {
-    return path.join(this.baseDir, this.imageDir);
-  }
-
-  private isImageFile(fileName: string): boolean {
-    const ext = path.extname(fileName).toLowerCase();
-    return this.allowedExtensions.includes(ext);
   }
 
   async upload(file: File, fileName: string, folderId: number | null = null) {
-    const ext = path.extname(file.name);
-
-    if (!this.allowedExtensions.includes(ext)) {
-      this.logger.error({ fn: 'upload', ext }, 'Unsupported file type');
-      throw new FileStorageError(400, 'Unsupported file type');
-    }
-
-    const nodeStream = webReadableToNode(file.stream());
+    const nodeStream = this.getNodeStream(file);
     const savedPath = await this.saveImageStream(nodeStream, fileName);
 
     const resizedBuffer = await this.resizeImage(fileName, null, 150);
     if (resizedBuffer) {
-      const name = path.basename(fileName, path.extname(fileName));
-      const thumbnailFileName = `${name}_thumbnail${ext}`;
-      await this.saveImageBuffer(resizedBuffer, thumbnailFileName);
+      await this.saveImageBuffer(
+        resizedBuffer,
+        this.getThumbnailFileName(fileName),
+      );
     }
 
     let meta = this.findImageMetadataByFileName(fileName);
     if (meta) {
       this.removeImageMetadataByFileName(fileName);
     }
-    meta = await this.getImageMetadata(fileName);
+    const baseMeta = await this.getImageMetadata(fileName);
+
+    meta = {
+      ...baseMeta,
+      thumbnail: this.getThumbnailFileName(fileName),
+    };
+
     const savedFileName = this.saveImageMetadata(meta, folderId);
 
     this.logger.info(
@@ -90,98 +53,14 @@ export class ImageStorageService extends FileStorageService {
   async delete(fileName: string) {
     this.removeImageMetadataByFileName(fileName);
 
-    const ext = path.extname(fileName);
-    const name = path.basename(fileName, ext);
+    const filesToDelete = [fileName, this.getThumbnailFileName(fileName)];
 
-    const thumbnailFileName = `${name}_thumbnail${ext}`;
+    for (const file of filesToDelete) {
+      if (await this.exists(this.getFilePath(file))) {
+        await this.remove(file);
 
-    const filesToDelete = [fileName, thumbnailFileName];
-
-    for (const f of filesToDelete) {
-      const exists = await this.exists(this.getFilePath(f));
-      if (exists) {
-        await this.remove(f);
-        this.logger.info({ f, fn: 'delete' }, 'Image deleted successfully');
+        this.logger.info({ file, fn: 'delete' }, 'Image deleted successfully');
       }
-    }
-  }
-
-  async saveImageBuffer(buffer: Buffer, fileName: string) {
-    if (!this.isImageFile(fileName)) {
-      this.logger.error(
-        { fileName, fn: 'saveImageBuffer' },
-        'Unsupported file type',
-      );
-      throw new FileStorageError(400, 'Unsupported file type');
-    }
-    return this.saveBuffer(buffer, fileName);
-  }
-
-  async saveImageStream(stream: NodeJS.ReadableStream, fileName: string) {
-    if (!this.isImageFile(fileName)) {
-      this.logger.error(
-        { fileName, fn: 'saveImageStream' },
-        'Unsupported file type',
-      );
-      throw new FileStorageError(400, 'Unsupported file type');
-    }
-    return this.saveStream(stream, fileName);
-  }
-
-  private async getImageMetadata(fileName: string): Promise<ImageMetadata> {
-    const filePath = this.getFilePath(fileName);
-    if (!(await this.exists(filePath))) {
-      this.logger.error({ fileName, fn: 'getImageMetadata' }, 'File not found');
-      throw new FileStorageError(404, 'File not found');
-    }
-
-    const fileStats = await this.getFileStats(fileName);
-    const image = sharp(filePath);
-    const metadata = await image.metadata();
-    const name = path.basename(fileName, path.extname(fileName));
-
-    return {
-      name,
-      width: metadata.width ?? 0,
-      height: metadata.height ?? 0,
-      format: metadata.format ?? 'unknown',
-      size: fileStats.size,
-      fileName,
-      createdAt: fileStats.birthtime.getTime(),
-      thumbnail: `${name}_thumbnail${path.extname(fileName)}`,
-      mtime: fileStats.mtime.getTime(),
-    };
-  }
-
-  /**
-   * Changes the size of the image on disk.
-   * @param fileName - file name
-   * @param width - new width (if null, proportionally scaled by height)
-   * @param height - new height (if null, proportionally scaled by width)
-   * @param overwrite - if true, the original file will be overwritten
-   * @returns Promise<Buffer | undefined> - buffer of resized image
-   */
-  async resizeImage(
-    fileName: string,
-    width: number | null,
-    height: number | null,
-    overwrite = false,
-  ): Promise<Buffer | undefined> {
-    const filePath = this.getFilePath(fileName);
-
-    if (!(await this.exists(filePath))) {
-      this.logger.error({ fileName, fn: 'resizeImage' }, 'File not found');
-      throw new FileStorageError(404, 'File not found');
-    }
-
-    const image = sharp(filePath);
-    const resized = image.resize(width || undefined, height || undefined);
-
-    if (overwrite) {
-      await resized.toFile(filePath);
-      return;
-    } else {
-      return resized.toBuffer();
     }
   }
 
@@ -255,14 +134,18 @@ export class ImageStorageService extends FileStorageService {
 
     for (const file of withoutThumbnails) {
       if (!existing.has(file)) {
-        const meta = await this.getImageMetadata(file);
-        this.saveImageMetadata(meta);
+        const baseMeta = await this.getImageMetadata(file);
+
+        this.saveImageMetadata({
+          ...baseMeta,
+          thumbnail: this.getThumbnailFileName(baseMeta.fileName),
+        });
       }
     }
 
-    for (const video of this.listImageMetadata()) {
-      if (!files.includes(video.fileName)) {
-        this.removeImageMetadataByFileName(video.fileName);
+    for (const image of this.listImageMetadata()) {
+      if (!files.includes(image.fileName)) {
+        this.removeImageMetadataByFileName(image.fileName);
       }
     }
   }
