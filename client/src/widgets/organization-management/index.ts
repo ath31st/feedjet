c8e6d9ed6a11f28eea8edb66c0992dc94d4f0e26
ath31st <ath31st@yandex.ru>
2026-07-09 16:20:29 +0300

    empty management component

---
+++ client/src/widgets/organization-management/index.ts
@@ -0,0 +1,1 @@
+export { OrganizationManagementWidget } from './ui/OrganizationManagementWidget';
---
+++ client/src/widgets/organization-management/ui/OrganizationManagementWidget.tsx
@@ -0,0 +1,3 @@
+export function OrganizationManagementWidget() {
+  return <div>OrganizationManagementWidget</div>;
+}
