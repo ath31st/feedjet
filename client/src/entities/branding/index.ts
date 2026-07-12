c7473b24f193f2a0e6852e41713e7d19ad625586
ath31st <ath31st@yandex.ru>
2026-07-12 11:20:57 +0300

    transfer logo to branding entity

--- client/src/entities/branding/index.ts
+++ client/src/entities/branding/index.ts
@@ -1,6 +1,8 @@
 export * from '@shared/types/branding.config';
 export * from './api/useBranding';
 export * from './model/brandingConfigStore';
+export * from './api/useLogo';
+export * from './lib/buildLogoUrl';
 
 import type { BrandingConfig as BrandingConfigApi } from '@shared/types/branding.config';
 export type BrandingConfig = Omit<
--- client/src/widgets/branding-config-management/model/useLogo.ts
+++ client/src/widgets/branding-config-management/model/useLogo.ts
@@ -3,7 +3,7 @@ import {
   useDeleteLogo,
   useGetLogo,
   useUploadLogo,
-} from '@/entities/logo';
+} from '@/entities/branding';
 
 export const useLogo = () => {
   const { data: logo, isLoading } = useGetLogo();
