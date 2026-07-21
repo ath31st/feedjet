ef82dd9ee1023663e4608d6b489b27c8368e116d
ath31st <ath31st@yandex.ru>
2026-07-21 15:27:58 +0300

    remove extra envs for client

--- client/src/shared/lib/useEnv.ts
+++ client/src/shared/lib/useEnv.ts
@@ -1,19 +1,11 @@
-import {
-  COMPANY_NAME,
-  HEADER_TITLE,
-  LAT,
-  LOCATION_TITLE,
-  LON,
-} from '../config';
+import { LAT, LOCATION_TITLE, LON } from '../config';
 
 export function useEnv() {
   const lat = Number(LAT);
   const lon = Number(LON);
 
   return {
-    headerTitle: HEADER_TITLE ?? '',
     locationTitle: LOCATION_TITLE ?? '',
-    companyName: COMPANY_NAME ?? '',
     locationLat: Number.isNaN(lat) ? 0 : lat,
     locationLon: Number.isNaN(lon) ? 0 : lon,
   };
